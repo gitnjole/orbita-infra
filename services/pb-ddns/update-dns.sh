3562f7d55920feb4a53d4da42d3ef98d3a86f4c1
Antonio <cahunantonio1@gmail.com>
2025-04-19 13:03:33 +0200

    add dns config files

---
+++ services/pb-ddns/update-dns.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+if [ -f .env ]; then
+  export $(grep -v '^#' .env | xargs)
+fi
+
+mkdir -p "$(dirname "$LOG_FILE")"
+
+log() {
+  echo "$(date '+%Y-%m-%d %H:%M:%S') - $1" >> "$LOG_FILE"
+}
+
+CURRENT_IP=$(curl -s https://api.ipify.org)
+
+if [[ -f "$LAST_IP_FILE" ]]; then
+  LAST_IP=$(<"$LAST_IP_FILE")
+  if [[ "$CURRENT_IP" == "$LAST_IP" ]]; then
+    exit 0
+  fi
+fi
+
+echo "$CURRENT_IP" > "$LAST_IP_FILE"
+
+cat > /tmp/porkbun-data.json << EOF
+{
+  "apikey": "$API_KEY",
+  "secretapikey": "$SECRET_KEY",
+  "content": "$CURRENT_IP",
+  "ttl": "600"
+}
+EOF
+
+RESPONSE_ROOT=$(curl -s -X POST "https://api.porkbun.com/api/json/v3/dns/editByNameType/$DOMAIN/A/" \
+  -H "Content-Type: application/json" \
+  -d @/tmp/porkbun-data.json)
+log "Root domain API Response: $RESPONSE_ROOT"
+
+RESPONSE_SUB=$(curl -s -X POST "https://api.porkbun.com/api/json/v3/dns/editByNameType/$DOMAIN/A/$SUBDOMAIN" \
+  -H "Content-Type: application/json" \
+  -d @/tmp/porkbun-data.json)
+log "Subdomain API Response: $RESPONSE_SUB"
+
+RESPONSE_DSUB=$(curl -s -X POST "https://api.porkbun.com/api/json/v3/dns/editByNameType/$DOMAIN/A/$DEMO_SUBDOMAIN" \
+  -H "Content-Type: application/json" \
+  -d @/tmp/porkbun-data.json)
+log "Demo Subdomain API Response: $RESPONSE_DSUB" 
+
+rm /tmp/porkbun-data.json
